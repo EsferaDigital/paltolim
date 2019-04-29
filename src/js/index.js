4426aa66ec8a3ff21eb18fc6de644e3403a88e01
Gabriel Zavando <gabrielzavando@gmail.com>
2019-04-29 19:29:07 -0400

    primer commit

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+import {toggleNav} from './dev/nav';
+// import Masonry from './dev/masonry';
+// import SliderFade from '../dev/slider';
+// import TabsNav from '../dev/tabs';
+// import carruselNormal from '../dev/slider';
+// import cuentaRegresiva from '../dev/cuenta-regresiva';
+
+toggleNav()
+// Masonry()
+// scrollNav()
+// carruselNormal()
+// TabsNav()
+// SliderFade()
+// cuentaRegresiva()
